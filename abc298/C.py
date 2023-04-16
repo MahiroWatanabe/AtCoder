a9048db247c842af198ea8ed55fa3c9ae137b095
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-16 10:30:27 +0900

    abc298

--- abc298/C.py
+++ abc298/C.py
@@ -1,16 +1,16 @@
-MOD = 998244353
+N = int(input())
 Q = int(input())
-S = [1]  # スタックに最初は1を入れておく
+M = 200001
+box = [[] for _ in range(N+1)]
+card = [set() for _ in range(M)]
+
 for _ in range(Q):
-    query = list(map(int, input().split()))
-    if query[0] == 1:
-        S.append(query[1])
-    elif query[0] == 2:
-        S.pop(0)
+    q = list(map(int, input().split()))
+    if q[0] == 1:
+        i, j = q[1:]
+        box[j].append(i)
+        card[i].add(j)
+    elif q[0] == 2:
+        print(*sorted(box[q[1]]))
     else:
-        # スタックの要素を10進数表記の数と見なして計算する
-        n = len(S)
-        res = 0
-        for i in range(n):
-            res = (res * 10 + S[i]) % MOD
-        print(res)
+        print(*sorted(card[q[1]]))
--- abc298/D.py
+++ abc298/D.py
@@ -1,15 +1,27 @@
+import sys, re, string
+from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
+from copy import deepcopy, copy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+
 Q = int(input())
-S = 1
 MOD = 998244353
+q = deque()
+q.append(1)
+r = 1
+
 for _ in range(Q):
-    x,*_a = map(int, input().split())
-    if x == 1:
-        v = _a[0]
-        S = S * 10 + v
-    elif x == 2:
-        S = S % (10**(len(str(S))-1))
-        S %= MOD
+    s = list(map(int,input().split()))
+    if s[0] == 1:
+        x = s[1]
+        q.append(x)
+        r = (r * 10 + x) % MOD
+    elif s[0] == 2:
+        y = q.popleft()
+        r = (r - y*pow(10,len(q),MOD)) % MOD
     else:
-        S %= MOD
-        print(S)
-    print("A",str(S))
+        print(r)
