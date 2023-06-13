83f1a5aafa2705cbf607d691fd56fca963f1000e
MahiroWatanabe <konchikuwa2@gmail.com>
2023-06-13 22:59:11 +0900

    abc300

--- abc300/D.py
+++ abc300/D.py
@@ -30,3 +30,43 @@ for i in range(len(primes)):
             break
         ans += prefix_sum[isqrt(N // (a * a * b))] - prefix_sum[b]
 print(ans)
+
+# import sys, re, string
+# from math import ceil, floor, sqrt, isqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
+# from copy import deepcopy, copy
+# from collections import Counter, deque, defaultdict
+# from heapq import heapify, heappop, heappush
+# from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
+# from bisect import bisect, bisect_left, bisect_right
+# from functools import reduce
+# from decimal import Decimal, getcontext
+
+# def erasto(N):
+#     isPrime = [True] * (N+1)
+#     isPrime[0] = isPrime[1] = False
+#     primes = []
+
+#     for i in range(2,N+1):
+#         if isPrime[i]:
+#             primes.append(i)
+#             for j in range(i*i,N+1,i):
+#                 isPrime[j] = False
+                
+#     return isPrime,primes
+
+# N = int(input())
+# isPrime,primes = erasto(10**6)
+# ans = 0
+
+# for a in range(len(primes)):
+#     if pow(primes[a],5) > N:
+#         break
+#     for b in range(a+1,len(primes)):
+#         if pow(primes[a],2)*pow(primes[b],3) > N:
+#             break
+#         for c in range(b+1,len(primes)):
+#             if pow(primes[a],2)*primes[b]*pow(primes[c],2) > N:
+#                 break
+#             ans += 1
+
+# print(ans)
