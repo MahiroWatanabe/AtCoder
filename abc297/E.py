78ff0367fa14dfb5c7cad77269e096b1ef89633a
MahiroWatanabe <konchikuwa2@gmail.com>
2023-06-15 23:04:20 +0900

    abc297

---
+++ abc297/E.py
@@ -0,0 +1,27 @@
+import sys, re, string
+from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
+from copy import deepcopy, copy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
+from bisect import bisect, bisect_left, bisect_right, insort_left, insort_right
+from functools import reduce
+from decimal import Decimal, getcontext
+
+N,K = map(int, input().split())
+A = list(map(int,input().split()))
+D = [i for i in A]
+S = set()
+for i in A:
+    S.add(i)
+heapify(D)
+
+for i in range(K-1):
+    v = heappop(D)
+    for j in range(N):
+        num = v+A[j]
+        if num not in S:
+            heappush(D,num)
+            S.add(num)
+
+print(min(D))
