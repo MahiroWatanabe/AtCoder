9263e256b0946686d0c8e2bdc5cc4e8a5d249099
MahiroWatanabe <konchikuwa2@gmail.com>
2023-06-20 22:41:36 +0900

    abc291

--- abc291/E.py
+++ abc291/E.py
@@ -1,27 +1,31 @@
 import sys, re, string
-from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
-from copy import deepcopy
+from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
+from copy import deepcopy, copy
 from collections import Counter, deque, defaultdict
 from heapq import heapify, heappop, heappush
 from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
-from bisect import bisect, bisect_left, bisect_right
+from bisect import bisect, bisect_left, bisect_right, insort_left, insort_right
 from functools import reduce
 from decimal import Decimal, getcontext
 
 def topological_sort(point,num):
-    Que = []
+    Que = deque()
     for i in range(len(point)):
         if num[i] == 0:
-            heappush(Que,i)
-    ans = []
-    
+            Que.append(i)
+    ans = [0]*N
+    na = N
     while Que:
-        v = heappop(Que)
-        ans.append(v+1)
+        if len(Que) > 1:
+            print("No")
+            exit()
+        v = Que.popleft()
+        ans[v] = na
+        na -= 1
         for nv in point[v]:
             num[nv] -= 1
             if num[nv] == 0:
-                heappush(Que,nv)
+                Que.append(nv)
                 
     if len(ans) == N:
         print("Yes")
@@ -33,9 +37,9 @@ N,M = map(int, input().split())
 point = [[] for _ in range(N)]
 num = [0]*N
 for _ in range(M):
-    X,Y = map(int, input().split())
-    X,Y = X-1,Y-1
-    point[X].append(Y)
-    num[Y] += 1
+    x,y = map(int, input().split())
+    x,y = x-1,y-1
+    point[y].append(x)
+    num[x] += 1
 
 topological_sort(point,num)
