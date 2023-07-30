fd18b0af6513bfce236ac81b0edac780ec6124ee
MahiroWatanabe <konchikuwa2@gmail.com>
2023-07-30 10:33:51 +0900

    abc312

--- abc312/C.py
+++ abc312/C.py
@@ -1,66 +1,21 @@
-import sys, re, string
-from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
-from copy import deepcopy, copy
-from collections import Counter, deque, defaultdict
-from heapq import heapify, heappop, heappush
-from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
-from bisect import bisect, bisect_left, bisect_right, insort_left, insort_right
-from functools import reduce, cmp_to_key
-from decimal import Decimal, getcontext
-
 N,M = map(int, input().split())
-A = sorted(list(map(int,input().split())))
-B = sorted(list(map(int,input().split())))
-left = 1
-right = 10**10
-print(bisect_right(A,9999))
-print(bisect_right(B,9999))
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+L,R = 0,10**9
 
-while right-left > 1:
-    mid = (left+right)//2
-    print(bisect_right(A,mid) ,bisect_right(B,mid))
-    if bisect_right(A,mid) >= bisect_right(B,mid):
-        left = mid
+while R-L > 1:
+    mid = (L+R)//2
+    na = 0
+    nb = 0
+    for i in range(N):
+        if A[i] <= mid:
+            na += 1
+    for i in range(M):
+        if B[i] >= mid:
+            nb += 1
+    if na >= nb:
+        R = mid
     else:
-        right = mid
+        L = mid
 
-print(left)
-
-# import sys, re, string
-# from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf, cos, sin
-# from copy import deepcopy, copy
-# from collections import Counter, deque, defaultdict
-# from heapq import heapify, heappop, heappush
-# from itertools import accumulate, product, combinations, combinations_with_replacement, permutations,groupby
-# from bisect import bisect, bisect_left, bisect_right, insort_left, insort_right
-# from functools import reduce, cmp_to_key
-# from decimal import Decimal, getcontext
-
-# def find_min_price(N, M, A, B):
-#     C,D = [],[]
-#     for i in A:
-#         C.append(i)
-#         C.append(i+1)
-#     for i in B:
-#         D.append(i)
-#         D.append(i+1)
-#     E = sorted(set(C+D))
-#     n,m = max(A),max(B)
-#     for i in range(len(E)):
-        
-#         a = bisect_right(A,E[i])
-#         b = bisect_right(B,E[i])
-#         # if :
-#         #     print(E[i])
-#         #     exit()
-#         if (E[i] > n or E[i] > m) or a >= M-b:
-#             print(E[i])
-#             exit()
-
-# N, M = map(int, input().split())
-# A = sorted(list(map(int, input().split())))
-# B = sorted(list(map(int, input().split())))
-
-# result = find_min_price(N, M, A, B)
-
-# print(result)
+print(R)
