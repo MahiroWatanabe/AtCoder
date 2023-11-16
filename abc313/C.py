117a44a188f84651277f79f140003d9122de14a7
mahiroMac <konchikuwa2@gmail.com>
2023-11-16 19:00:11 +0900

    abc313

--- abc313/C.py
+++ abc313/C.py
@@ -1,9 +1,20 @@
+# 1増やして1減らすので、数列の合計が変わらない
+# 数列に加算減算することで、平均に近づけていき、余りの数を数列に分散して加算する
+
 N = int(input())
-A = list(map(int,input().split()))
-temp = sum(A)//N
+A = list(map(int, input().split()))
+tmp = sum(A) // N
+rem = sum(A) % N
+B = [tmp for _ in range(N)]
+
+for i in range(rem):
+    B[i] += 1
+
+A.sort()
+B.sort()
+
 ans = 0
+for i in range(N):
+    ans += abs(A[i] - B[i])
 
-for i in A:
-    ans += min(abs(temp-i),abs(temp+1-i))
-
-print(ans//2 if ans%2 == 0 else ans//2+1)
+print(ans // 2)
