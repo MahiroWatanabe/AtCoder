6c46c5e7b64e2a2a320e7ce52d54356d088952b1
mahiroMac <konchikuwa2@gmail.com>
2023-11-18 23:29:19 +0900

    abc329

--- abc329/C.py
+++ abc329/C.py
@@ -18,19 +18,14 @@ from decimal import Decimal, getcontext
 N = int(input())
 S = input()
 abc = [chr(ord("a") + i) for i in range(26)]
-D = set()
 E = defaultdict(int)
 
 for i in abc:
-    # print(i)
     current = ""
     for j in range(N):
         if i != S[j]:
-            # print("AAA")
             if current == "":
                 continue
-            # D.add(current)
-            # print(current)
             E[i] = max(E[i], len(current))
             current = ""
         else:
@@ -38,9 +33,6 @@ for i in abc:
     if current != "":
         E[i] = max(E[i], len(current))
 
-# print(D)
-# print(len(D)
-# print(E)
 cnt = 0
 for i, j in E.items():
     cnt += j
